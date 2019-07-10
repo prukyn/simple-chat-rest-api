6f2cd38bfccf21532f7027debd6c8dfcfed7a55b
prukyn <rukyn.petro@gmail.com>
2019-07-11 01:25:25 +0300

    Added worked serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,24 @@
+import re
 from rest_framework import serializers
 from django.core.exceptions import ObjectDoesNotExist
 from .models import ChatUser, Message
 
 
 class ChatUserSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    '''
+    Serializer that represent chatuser
+    using CharField, because must validate email field by regex
+    '''
+    email = serializers.RegexField(r'^[^@]+@[^@]+\.[^@]+$')
 
 
-
-class MessageSerializer(serializers.Serializer):    
-    text = serializers.CharField(max_length=100)
+class MessageSerializer(serializers.Serializer):
+    text = serializers.RegexField(r'[\w|\W]{1,100}')
+    created_date = serializers.DateTimeField(read_only=True)
     author = ChatUserSerializer()
-    create_date = serializers.DateTimeField(required=False)
+
+    def create(self, validated_data):
+        author_data = validated_data.pop('author')
+        user = ChatUser.objects.get_or_create(email=author_data['email'])[0]
+        message = Message.objects.create(author=user, **validated_data)
+        return message
